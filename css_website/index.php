f2169e6ff296493d707655cd411710ff9c578535
yapsterr <60122561+yapsterr@users.noreply.github.com>
2020-10-06 20:29:17 +0800

    Add files via upload

---
+++ css_website/index.php
@@ -0,0 +1,96 @@
+<html>
+    <head>
+        <title>Food Wars</title>
+        <link rel="stylesheet" type="text/css" href="css/container.css">
+        <link rel="stylesheet" type="text/css" href="css/header.css">
+        <link rel="stylesheet" type="text/css" href="css/content.css">
+        <link rel="stylesheet" type="text/css" href="css/footer.css">
+    </head>
+        <body>
+            <div id="container">
+                <div id="header">
+                    <div class="ice-cream_wrap">
+                        <div class="ice-cream">
+                            <div class="stick"></div>
+                        </div>
+                    </div>
+                        <div id="logo">
+                            <span class="text">Food Wars</span>
+                        </div>
+                </div>
+
+            <div id="content">
+                        <div class="box">
+                    <h2>Breakfast</h2>
+                    <p>It was not until the 15th century that "breakfast" came into use in written English to describe a morning meal, which literally means to break the fasting period of the prior night; in 
+                        Old English the term was morgenmete meaning "morning meal."
+                        <div id="egg_wrap">
+                            <div id="eggcontainer">
+                                <div id="egg1"></div>
+                                <div id="egg2"></div>
+                                <div id="egg3"></div>
+                                <div id="yolk"></div>
+                            </div>
+                        </div>
+                    </p>
+                </div>
+                <div class="box">
+                    <h2>Dessert</h2>
+                    <p> The term dessert can apply to many confections, 
+                        such as biscuits, cakes, cookies, custards, gelatins, 
+                        ice creams, pastries, pies, puddings, sweet soups, and tarts. 
+                        Fruit is also commonly found in dessert courses because of its naturally occurring sweetness.
+                    </p>
+                    <div id="dessert_wrap">
+                        <div class="dessert">
+                            <div class="donut">
+                                <div class="cream"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="box">
+                    <h2>Beverages</h2>
+                    <p>  is a liquid intended for human consumption. 
+                        In addition to their basic function of satisfying thirst, drinks play important roles in human culture. 
+                        Common types of drinks include plain drinking water, milk, coffee, tea, hot chocolate, juice and soft drinks.
+                    </p>
+                    <div id="drink">
+                        <div class="bottle">
+                            <div class="neck"></div>
+                        </div>
+                        <div class="glass">
+                            <div class="stand">
+                                <div class="base"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+                <div id="footer">
+                    <div class="icon">
+                       <ul class="social-icons">
+                            <li><a href="" class="social-icon" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="" class="social-icon" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+                            <li><a href="" class="social-icon" title="Youtube"><i class="fa fa-youtube"></i></a></li>
+                            <li><a href="" class="social-icon" title="Google+"><i class="fa fa-google-plus"></i></a></li>
+                       </ul>
+                       <p>©Copyright.All rights reserved</p>
+                    </div>
+                    <div class="bubble">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                </div>
+            </div>
+        </body>
+</html>
